f3de5808e5de89a603b30e2d87d367e0de18d065
Narasimha271 <simha93930@gmail.com>
2026-08-27 19:14:32 +0100

    day 3

---
+++ Operators.py
@@ -0,0 +1,47 @@
+# Arthemetic Operators
+# + Used for addition
+# - used for subtraction
+# * is used for multiplication
+# / is used for division to return qutoent
+# % is used for division reminder (Modulus) 
+# // is used for floor division to return qutoent in integers()
+# ** is used for defining power
+
+
+
+#Assignment Operators
+# basic assignment operator =, it assigns the value on right to Left
+# += , it adds the value on the left with right and assign it to the left
+# -=, it subtracts the value on the left with right and assign it to the left
+# *=, it multiplies the value on the left with right and assign it to the left
+# /=, it divides the value on the left with right and assign it to the left
+# %=, it returns reminder the value on the left with right and assign it to the left
+
+a = 3
+b = 7
+c = 2
+d  = 3
+e = c**d
+print(e)
+b += a # b = b + a 
+print(b)  
+
+
+# comparision Operators 
+# ==, this is used to Equal 
+# !=, this is used to not equal comparision
+# > , this is used to Greater than
+# < , this is used to Less than
+# >=, this is used to Greater than or equal to
+# <=, this is used to Less than or equal to
+
+
+# Membership Operators 
+# in - is used to check if the left value is in right
+# not in - is used to check if the left value is not in right
+test = "This is a test for Membershop operators"
+
+TrueOrFalse1 = "in" in test
+TrueOrFalse2 = "Membershop" in test
+print(TrueOrFalse1)
+print(TrueOrFalse2)
