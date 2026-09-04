0da72875a62ea2f35bfa11aad2bef9611b6750ff
emadhuripriya-code <e.madhuripriya@gmail.com>
2026-09-04 22:24:11 +0530

    bfghbdf

--- MadHW4.py
+++ MadHW4.py
@@ -9,26 +9,28 @@ stud1 = [name, age, fav_sub, marks]
 
 print('----- List -----\n\n')
 
-print(f"Student Details:  {stud1}\n")
-print(f"First item: {stud1[0]}\n")
-print(f"Last item: {stud1[-1]}\n")
-print(f"First 2 item: {stud1[0:2]}\n")
-print(f"Last 2 item: {stud1[-2: ]}\n")
-print(f"Total items: {len(stud1)}\n")
+print(f"Student Details:  {stud1}")
+print(f"First item: {stud1[0]}")
+print(f"Last item: {stud1[-1]}")
+print(f"First 2 item: {stud1[0:2]}")
+print(f"Last 2 item: {stud1[-2: ]}")
+print(f"Total items: {len(stud1)}")
 
 print('----- Data Types -----\n\n')
 
-print(f"The complete list: {type(stud1)}\n")
-print(f"Name: {type(stud1[0])}\n")
-print(f"Age: {type(stud1[1])}\n")
-print(f"Favourite Subject: {type(stud1[2])}\n")
-print(f"Marks: {type(stud1[3])}\n")
+print(f"The complete list: {type(stud1)}")
+print(f"Name: {type(stud1[0])}")
+print(f"Age: {type(stud1[1])}")
+print(f"Favourite Subject: {type(stud1[2])}")
+print(f"Marks: {type(stud1[3])}")
 
-fav_sub= 'Python'
+stud1[2]= 'Python'
+print(stud1)
 stud1[1:3] = [30, "Computer Science"]
+print(stud1)
 stud1[-1:-1] = ["UPSC", "Software Development"]
 
-print(f"Replaced list: {stud1}\n\n")
+print(f"Replaced list: {stud1}")
 
 
 if marks >= 80:
@@ -41,4 +43,7 @@ else:
     result='Fail'
 
 print(f"----- Final Output -----\n\n")
-print(f"Name: {name} \nAge: {age} \nFavourite Subject: {fav_sub} \nMarks: {marks} \nResult: {result}\n")
+print(f"Name: {name} \nAge: {age} \nFavourite Subject: {fav_sub} \nMarks: {marks} \nResult: {result}")
+
+print(f"----- Final Output 2 -----\n\n")
+print(f"Name: {stud1[0]} \nAge: {stud1[1]} \nFavourite Subject: {stud1[2:5]} \nMarks: {stud1[-1]} \nResult: {result}")
